396ed3ec21f7a701709fdacffa45119f1fc6bfe8
Atlantis-GitH <kato.kid75@gmail.com>
2024-10-13 04:31:17 +0200

    Mise à jour fichier de check update
    Mise à jour fichier de check update pour être raccord avec le fichier maitre

--- JVCForumRollback.meta.js
+++ JVCForumRollback.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JVCForumRollback
 // @namespace    https://github.com/Roadou
-// @version      2.8.5
+// @version      2.9.0
 // @description  Ancienne page des forums JVC
 // @author       IceFairy, Atlantis
 // @match        *://www.jeuxvideo.com/forums.htm
