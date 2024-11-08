6d65db65f0161d6acd885cffa6bb035e732369e5
Atlantis-GitH <184443362+Atlantis-GitH@users.noreply.github.com>
2024-11-08 08:41:29 +0100

    meta 6.5.4 (maj)
    meta 6.5.4 (maj)

--- JVCForumRollback.meta.js
+++ JVCForumRollback.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JVCForumRollback
 // @namespace    https://github.com/Roadou
-// @version      6.5.3
+// @version      6.5.4
 // @description  Ancienne page des forums JVC
 // @author       IceFairy, Atlantis
 // @match        *://www.jeuxvideo.com/forums.htm
