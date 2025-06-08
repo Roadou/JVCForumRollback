5eda49f2b041e4dc6de6ad243cf8d44890f266f6
Lantea-Git <184443362+Lantea-Git@users.noreply.github.com>
2025-06-08 05:32:57 +0200

    6.9.7 meta
    6.9.7 meta

--- JVCForumRollback.meta.js
+++ JVCForumRollback.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JVCForumRollback
 // @namespace    https://github.com/Roadou
-// @version      6.9.6
+// @version      6.9.7
 // @description  Ancienne page des forums JVC
 // @author       IceFairy, Atlantis
 // @match        *://www.jeuxvideo.com/forums.htm
